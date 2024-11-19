774ce8d7e2f988785d1182548250e783585eaf51
Aaro Rissanen <aaro.rissanen16@gmail.com>
2024-11-19 23:05:54 +0200

    +cross-platform with C11 threads, bugfixes -posix threads

--- at.h
+++ at.h
@@ -4,58 +4,74 @@
  * @author github.com/R1ssanen
  */
 
-#ifndef AT_H
-#define AT_H
+#ifndef _AT_H
+#define _AT_H
 
-#include <pthread.h>
-#include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <threads.h>
+#include <time.h>
 
-typedef _Bool    b8;
-typedef int8_t   i8;
-typedef int16_t  i16;
-typedef int32_t  i32;
-typedef int64_t  i64;
-typedef uint8_t  u8;
-typedef uint16_t u16;
-typedef uint32_t u32;
-typedef uint64_t u64;
-typedef float    f32;
-typedef double   f64;
+typedef _Bool         b8;
+typedef int           i32;
+typedef long          i64;
+typedef unsigned int  u32;
+typedef unsigned long u64;
+typedef double        f64;
 
 #define true  1
 #define false 0
-#define _at_FORWARD_DECL
+#define _AT_FORWARD_DECL
 
-#define _at_LOCK(mtx)   pthread_mutex_lock(&mtx)
-#define _at_UNLOCK(mtx) pthread_mutex_unlock(&mtx)
+typedef mtx_t _at_mutex_t;
+#define _AT_MUTEX_CREATE(mtx)  mtx_init(&mtx, mtx_plain)
+#define _AT_MUTEX_DESTROY(mtx) mtx_destroy(&mtx)
+#define _AT_LOCK(mtx)          mtx_lock(&mtx)
+#define _AT_UNLOCK(mtx)        mtx_unlock(&mtx)
 
-typedef pthread_mutex_t _at_mutex_t;
-typedef pthread_t       _at_pid_t;
+typedef thrd_t _at_pid_t;
+#define _AT_THREAD_CREATE(pid, fn, args) thrd_create(&pid, fn, args)
+#define _AT_THREAD_JOIN(pid)             thrd_join(pid, NULL)
+#define _AT_THREAD_SLEEP(duration)       thrd_sleep(&duration, NULL)
+#define _AT_THREAD_WHICH()               thrd_current()
+
+typedef enum {
+    AT_SUCCESS = 0,
+    AT_NULL_INPUT,
+    AT_SYS_ERROR,
+} at_error_t;
+
+/**
+ * @brief Get brief explanation string of error code.
+ * @param err Error to get information of.
+ * @return
+ */
+static inline const char* atGetErrorString(at_error_t err) {
+    switch (err) {
+    case AT_SUCCESS: return "(AT) No errors.";
+    case AT_NULL_INPUT: return "(AT) Invalid null function argument.";
+    case AT_SYS_ERROR: return "(AT) C-standard library function returned an error.";
+    default: return "(AT) Unknown error code.";
+    }
+}
 
 typedef struct atTask   atTask;
 typedef struct atThread atThread;
 typedef struct atPool   atPool;
 typedef struct _atQNode _atQNode;
 typedef struct _atQueue _atQueue;
-typedef void* (*at_task_func_t)(void*);
+typedef at_error_t (*at_task_func_t)(void*);
 
-typedef enum {
-    AT_SUCCESS = 0,
-    AT_NULL_INPUT,
-    AT_SYS_ERROR,
-    AT_LIB_ERROR,
-} at_error_t;
-
-static inline const char* atGetErrorString(at_error_t err) {
-    switch (err) {
-    case AT_SUCCESS: return "(AT) No errors.";
-    case AT_NULL_INPUT: return "(AT) Invalid null function argument.";
-    case AT_SYS_ERROR: return "(AT) C-standard function returned an error.";
-    case AT_LIB_ERROR: return "(AT) External library function returned an error.";
-    default: return "(AT) Unknown error code.";
-    }
+/**
+ * @brief Convenience function to create a timespec from seconds.
+ * @param seconds Seconds to create the timespec from.
+ * @return
+ */
+static inline struct timespec _atCreateTime(f64 seconds) {
+    struct timespec time;
+    time.tv_sec  = (i64)(seconds);
+    time.tv_nsec = (i64)(seconds - time.tv_sec) * 1E9L;
+    return time;
 }
 
 /*
@@ -89,10 +105,12 @@ struct _atQueue {
  * @return
  */
 static inline _atQueue _atCreateQueue(void) {
-    return (_atQueue){ .lock = PTHREAD_MUTEX_INITIALIZER, .front = NULL, .back = NULL };
+    _atQueue queue = { .front = NULL, .back = NULL };
+    _AT_MUTEX_CREATE(queue.lock);
+    return queue;
 }
 
-atThread* _at_FORWARD_DECL _atQDequeue(_atQueue*);
+atThread* _AT_FORWARD_DECL _atQDequeue(_atQueue*);
 
 /**
  * (internal)
@@ -105,8 +123,7 @@ static inline at_error_t _atDestroyQueue(_atQueue* queue) {
 
     while (_atQDequeue(queue));
 
-    if (pthread_mutex_destroy(&queue->lock)) { return AT_LIB_ERROR; }
-
+    _AT_MUTEX_DESTROY(queue->lock);
     return AT_SUCCESS;
 }
 
@@ -123,9 +140,9 @@ static inline b8 _atQIsEmpty(_atQueue* queue, at_error_t* err) {
         return false;
     }
 
-    _at_LOCK(queue->lock);
+    _AT_LOCK(queue->lock);
     b8 is_empty = (!queue->front) && (!queue->back);
-    _at_UNLOCK(queue->lock);
+    _AT_UNLOCK(queue->lock);
 
     if (err) { *err = AT_SUCCESS; }
     return is_empty;
@@ -147,17 +164,17 @@ static inline at_error_t _atQEnqueue(_atQueue* queue, atThread* thread) {
     node->thread = thread;
     node->next   = NULL;
 
-    _at_LOCK(queue->lock);
+    _AT_LOCK(queue->lock);
     {
         if (!queue->back) {
             queue->front = node;
-            queue->back  = node;
         } else {
             queue->back->next = node;
-            queue->back       = node;
         }
+
+        queue->back = node;
     }
-    _at_UNLOCK(queue->lock);
+    _AT_UNLOCK(queue->lock);
 
     return AT_SUCCESS;
 }
@@ -166,24 +183,24 @@ static inline at_error_t _atQEnqueue(_atQueue* queue, atThread* thread) {
  * (internal)
  * @brief Dequeue thread from queue.
  * @param queue Queue to dequeue from.
- * @return
+ * @return Null for empty queue or error.
  */
 atThread* _atQDequeue(_atQueue* queue) {
     if (!queue || _atQIsEmpty(queue, NULL)) { return NULL; }
 
     _atQNode* old_front = NULL;
 
-    _at_LOCK(queue->lock);
+    _AT_LOCK(queue->lock);
     {
         old_front    = queue->front;
         queue->front = queue->front->next;
         if (!queue->front) { queue->back = NULL; }
     }
-    _at_UNLOCK(queue->lock);
+    _AT_UNLOCK(queue->lock);
 
-    atThread* old_front_data = old_front->thread;
+    atThread* old_front_thread = old_front->thread;
     free(old_front);
-    return old_front_data;
+    return old_front_thread;
 }
 
 /*
@@ -195,13 +212,12 @@ atThread* _atQDequeue(_atQueue* queue) {
  * @brief Thread task return type.
  */
 struct atTask {
-    _at_mutex_t    lock;
-    at_task_func_t fn;
-    void*          args;
-    void*          ret;
-    u32            update_freq_ms;
-    _at_pid_t      pid; //! Thread associated with the task.
-    b8             done;
+    _at_mutex_t     lock;
+    struct timespec update_freq;
+    at_task_func_t  fn;
+    void*           args;
+    at_error_t      err;
+    b8              done;
 };
 
 /**
@@ -209,18 +225,20 @@ struct atTask {
  * @brief Create a task.
  * @param fn Function to execute.
  * @param args Arguments to the function.
- * @param update_freq_ms Frequence in which task checks whether it's
- *                       been completed when awaiting, in seconds.
+ * @param update_freq Frequency in which task checks whether it's
+ *                    been completed when awaiting, in seconds.
  * @return
  */
 static inline atTask _atCreateTask(at_task_func_t fn, void* args, f64 update_freq) {
-    return (atTask){ .lock           = PTHREAD_MUTEX_INITIALIZER,
-                     .fn             = fn,
-                     .args           = args,
-                     .ret            = NULL,
-                     .update_freq_ms = (u32)(update_freq * 1000.0),
-                     .done           = false };
+    atTask task = {
+        .update_freq = _atCreateTime(update_freq), .fn = fn, .args = args, .done = false
+    };
+
+    _AT_MUTEX_CREATE(task.lock);
+    return task;
 }
+
+at_error_t _AT_FORWARD_DECL atAwaitTask(atTask*);
 
 /**
  * (internal)
@@ -231,44 +249,37 @@ static inline atTask _atCreateTask(at_task_func_t fn, void* args, f64 update_fre
 static inline at_error_t _atDestroyTask(atTask* task) {
     if (!task) { return AT_NULL_INPUT; }
 
-    if (pthread_mutex_destroy(&task->lock)) { return AT_LIB_ERROR; }
+    at_error_t err = atAwaitTask(task);
+    if (err) { return err; }
 
-    task->fn   = NULL;
-    task->args = NULL;
-    task->ret  = NULL;
-    task->pid  = 0;
-
+    _AT_MUTEX_DESTROY(task->lock);
     return AT_SUCCESS;
 }
 
-atTask* _at_FORWARD_DECL atLaunchTask(at_task_func_t, void*, atThread*);
+atTask* _AT_FORWARD_DECL atLaunchTask(at_task_func_t, void*, atThread*);
 
-atTask* _at_FORWARD_DECL atScheduleTask(at_task_func_t, void*, atPool*);
+atTask* _AT_FORWARD_DECL atScheduleTask(at_task_func_t, void*, atPool*);
 
 /**
  * @brief Await a task. You must always await a launched or
  *        scheduled task before accessing its values to avoid a race condition.
  * @param task Task to be awaited.
- * @param err Optional error code output. Nullable.
+ * @return Error code returned from the task.
  */
-static inline void* atAwaitTask(atTask* task, at_error_t* err) {
-    if (!task) {
-        if (err) { *err = AT_NULL_INPUT; }
-        return NULL;
-    }
+at_error_t atAwaitTask(atTask* task) {
+    if (!task) { return AT_NULL_INPUT; }
 
-    _at_LOCK(task->lock);
+    _AT_LOCK(task->lock);
     {
         while (!task->done) {
-            _at_UNLOCK(task->lock);
-            usleep(task->update_freq_ms);
-            _at_LOCK(task->lock);
+            _AT_UNLOCK(task->lock);
+            _AT_THREAD_SLEEP(task->update_freq);
+            _AT_LOCK(task->lock);
         }
     }
-    _at_UNLOCK(task->lock);
+    _AT_UNLOCK(task->lock);
 
-    if (err) { *err = AT_SUCCESS; }
-    return task->ret;
+    return task->err;
 }
 
 /*
@@ -280,16 +291,16 @@ static inline void* atAwaitTask(atTask* task, at_error_t* err) {
  * @brief Thread structure.
  */
 struct atThread {
-    atTask      task;
-    _at_mutex_t lock;
-    atPool*     pool;
-    _at_pid_t   pid;
-    u32         update_freq_ms;
-    b8          exit;
-    b8          working;
+    atTask          task;
+    _at_mutex_t     lock;
+    struct timespec update_freq;
+    atPool*         pool;
+    _at_pid_t       pid;
+    b8              exit;
+    b8              working;
 };
 
-void* _at_FORWARD_DECL _atThreadLoop(void*);
+i32 _AT_FORWARD_DECL _atThreadLoop(void*);
 
 /**
  * @brief Create a thread.
@@ -303,16 +314,17 @@ void* _at_FORWARD_DECL _atThreadLoop(void*);
 static inline at_error_t atCreateThread(atPool* pool, f64 update_freq, atThread* thread) {
     if (!thread) { return AT_NULL_INPUT; }
 
-    *thread = (atThread){ .task           = _atCreateTask(NULL, NULL, update_freq),
-                          .lock           = PTHREAD_MUTEX_INITIALIZER,
-                          .pool           = pool,
-                          .update_freq_ms = (u32)(update_freq * 1000.0),
-                          .exit           = false,
-                          .working        = false };
+    *thread = (atThread){ .task        = _atCreateTask(NULL, NULL, update_freq),
+                          .pool        = pool,
+                          .update_freq = _atCreateTime(update_freq),
+                          .exit        = false,
+                          .working     = false };
 
-    if (pthread_create(&thread->pid, NULL, _atThreadLoop, thread)) { return AT_LIB_ERROR; }
+    if (_AT_MUTEX_CREATE(thread->lock) != thrd_success) { return AT_SYS_ERROR; }
+    if (_AT_THREAD_CREATE(thread->pid, _atThreadLoop, thread) != thrd_success) {
+        return AT_SYS_ERROR;
+    }
 
-    thread->task.pid = thread->pid;
     return AT_SUCCESS;
 }
 
@@ -324,32 +336,15 @@ static inline at_error_t atCreateThread(atPool* pool, f64 update_freq, atThread*
 static inline at_error_t atKillThread(atThread* thread) {
     if (!thread) { return AT_NULL_INPUT; }
 
-    _at_LOCK(thread->lock);
+    at_error_t err = _atDestroyTask(&thread->task);
+    if (err) { return err; }
+
+    _AT_LOCK(thread->lock);
     thread->exit = true;
-    _at_UNLOCK(thread->lock);
+    _AT_UNLOCK(thread->lock);
 
-    if (pthread_join(thread->pid, NULL)) { return AT_LIB_ERROR; }
-    if (pthread_mutex_destroy(&thread->lock)) { return AT_LIB_ERROR; }
-
-    at_error_t err = _atDestroyTask(&thread->task);
-    if (err) { return err; }
-
-    return AT_SUCCESS;
-}
-
-/**
- * @brief Forcefully kills a thread. Not as safe as 'atKillThread'.
- * @param thread Thread to be killed.
- * @return
- */
-static inline at_error_t atForceKillThread(atThread* thread) {
-    if (!thread) { return AT_NULL_INPUT; }
-
-    if (pthread_cancel(thread->pid)) { return AT_LIB_ERROR; }
-    if (pthread_mutex_destroy(&thread->lock)) { return AT_LIB_ERROR; }
-
-    at_error_t err = _atDestroyTask(&thread->task);
-    if (err) { return err; }
+    if (_AT_THREAD_JOIN(thread->pid) != thrd_success) { return AT_SYS_ERROR; }
+    _AT_MUTEX_DESTROY(thread->lock);
 
     return AT_SUCCESS;
 }
@@ -363,10 +358,10 @@ static inline at_error_t atForceKillThread(atThread* thread) {
  * @brief Thread pool structure.
  */
 struct atPool {
-    _atQueue  free;
-    atThread* pool;
-    u32       update_freq_ms;
-    u32       thread_count;
+    _atQueue        free;
+    struct timespec update_freq;
+    atThread*       pool;
+    u32             thread_count;
 };
 
 /**
@@ -383,9 +378,9 @@ static inline at_error_t atCreatePool(u32 thread_count, f64 update_freq, atPool*
     pool->pool = malloc(sizeof(atThread) * thread_count);
     if (!pool->pool) { return AT_SYS_ERROR; }
 
-    pool->update_freq_ms = (u32)(update_freq * 1000.0);
-    pool->thread_count   = thread_count;
-    pool->free           = _atCreateQueue();
+    pool->update_freq  = _atCreateTime(update_freq);
+    pool->thread_count = thread_count;
+    pool->free         = _atCreateQueue();
 
     at_error_t err;
     for (u64 i = 0; i < thread_count; ++i) {
@@ -429,10 +424,10 @@ static inline at_error_t atDestroyPool(atPool* pool) {
 atTask* atLaunchTask(at_task_func_t fn, void* args, atThread* thread) {
     if (!fn || !thread) { return NULL; }
 
-    _at_LOCK(thread->lock);
+    _AT_LOCK(thread->lock);
     {
         if (thread->working) {
-            _at_UNLOCK(thread->lock);
+            _AT_UNLOCK(thread->lock);
             return NULL;
         }
 
@@ -441,7 +436,7 @@ atTask* atLaunchTask(at_task_func_t fn, void* args, atThread* thread) {
         thread->task.done = false;
         thread->working   = true;
     }
-    _at_UNLOCK(thread->lock);
+    _AT_UNLOCK(thread->lock);
 
     return &thread->task;
 }
@@ -456,7 +451,7 @@ atTask* atLaunchTask(at_task_func_t fn, void* args, atThread* thread) {
 atTask* atScheduleTask(at_task_func_t fn, void* args, atPool* pool) {
     if (!pool) { return NULL; }
 
-    while (_atQIsEmpty(&pool->free, NULL)) { usleep(pool->update_freq_ms); }
+    while (_atQIsEmpty(&pool->free, NULL)) { _AT_THREAD_SLEEP(pool->update_freq); }
 
     atThread* free_thread = _atQDequeue(&pool->free);
     return atLaunchTask(fn, args, free_thread);
@@ -469,37 +464,36 @@ atTask* atScheduleTask(at_task_func_t fn, void* args, atPool* pool) {
 /**
  * (internal)
  * @brief Thread running loop.
- * @param _arg
+ * @param arg Thread handle.
  */
-void* _atThreadLoop(void* _arg) {
-    atThread* thread = (atThread*)_arg;
+i32 _atThreadLoop(void* arg) {
+    atThread* thread = (atThread*)arg;
     atTask*   task   = &thread->task;
 
-    _at_LOCK(thread->lock);
+    _AT_LOCK(thread->lock);
     {
         while (!thread->exit) {
             if (!thread->working) {
-                _at_UNLOCK(thread->lock);
-                usleep(thread->update_freq_ms);
+                _AT_UNLOCK(thread->lock);
+                _AT_THREAD_SLEEP(thread->update_freq);
             } else {
                 thread->working = false;
-                _at_UNLOCK(thread->lock);
+                _AT_UNLOCK(thread->lock);
 
-                task->ret = task->fn(task->args);
-
-                _at_LOCK(task->lock);
+                _AT_LOCK(task->lock);
+                task->err  = task->fn(task->args);
                 task->done = true;
-                _at_UNLOCK(task->lock);
+                _AT_UNLOCK(task->lock);
 
                 _atQEnqueue(&thread->pool->free, thread);
             }
 
-            _at_LOCK(thread->lock);
+            _AT_LOCK(thread->lock);
         }
     }
-    _at_UNLOCK(thread->lock);
+    _AT_UNLOCK(thread->lock);
 
-    return NULL;
+    return 0;
 }
 
 #endif
